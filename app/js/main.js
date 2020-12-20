8dbec511b5716b7ca1400a384de11068894df5a6
zitexx <zitexx@gmail.com>
2020-12-20 13:23:20 +0200

    add page product

--- app/js/main.js
+++ app/js/main.js
@@ -16,6 +16,18 @@ $(function () {
   })
 
 
+
+
+  $('.product-tabs__top-item').on('click', function (e) {
+    e.preventDefault() //удаление стандартного действия по ссылке
+    $('.product-tabs__top-item').removeClass('product-tabs__top-item--active')
+    $(this).addClass('product-tabs__top-item--active')
+
+    $('.product-tabs__content-item').removeClass('product-tabs__content-item--active')
+    $($(this).attr('href')).addClass('product-tabs__content-item--active')
+
+  })
+
   $('.shop-content__filter-btn').on('click', function () {
     $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active')
     $(this).addClass('shop-content__filter-btn--active')
